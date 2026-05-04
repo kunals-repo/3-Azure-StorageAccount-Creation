416fdbb38c5c3066de88fd1286a9875ef74c0abe
Kunal Yeole <kunalpatil9324@gmail.com>
2026-05-04 22:54:46 +0530

    updated storage acct variables declaration

--- variables.tf
+++ variables.tf
@@ -3,35 +3,26 @@ variable "storage-acct" {
     type = map(object({
       name = string
       replication = string
-      tags =  map(string)
+    
     }))
 
     default = {
       "storage1" = {
         name = "bmwstorage01"
         replication = "LRS"
-        tags = {
-            "owner" = "kunal yeole"
-            "use" = "spare parts logs"
-        }     
+         
       }
 
       "storage2" = {
         name = "bmwstorage02"
         replication = "ZRS"
-        tags = {
-            "owner" = "kunal yeole"
-            "use" = "sales logs"
-        } 
+        
       }
 
       "storage3" = {
         name = "bmwstorage03"
         replication = "ZRS"
-        tags = {
-            "owner" = "kunal yeole"
-            "use" = " data analystics logs"
-        }
+ 
       }
 
     }
