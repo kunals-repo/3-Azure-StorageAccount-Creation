ef51a2f06dbd811218982b5fb9090f62ccc8b3dd
Kunal Yeole <kunalpatil9324@gmail.com>
2026-05-04 22:12:10 +0530

    storage acct added

--- main.tf
+++ main.tf
@@ -1,58 +1,16 @@
 
 resource "azurerm_resource_group" "rg-block" {
-  name     = "vm-rg"
-  location = "centralindia"
+  name = "storage-rg"
+  location = "eastus"
 }
 
 
-resource "azurerm_virtual_network" "vnet-block" {
-  name                = var.vnet-name
-  address_space       = [var.vnet-ip]
-  location            = azurerm_resource_group.rg-block.location
-  resource_group_name = azurerm_resource_group.rg-block.name
+resource "azurerm_storage_account" "storage-block" {
+  name                     = "bmwacct0405"
+  resource_group_name      = azurerm_resource_group.rg-block.name
+  location                 = azurerm_resource_group.rg-block.location
+  account_tier             = "Premium"
+  account_replication_type = "LRS"
+  account_kind             = "BlockBlobStorage"
+
 }
-
-resource "azurerm_subnet" "subnet-block" {
-  name                 = var.subnet
-  resource_group_name  = azurerm_resource_group.rg-block.name
-  virtual_network_name = azurerm_virtual_network.vnet-block.name
-  address_prefixes     = [var.subnet-ip]
-}
-
-resource "azurerm_network_interface" "nic-block" {
-  name                = var.nic-name
-  location            = azurerm_resource_group.rg-block.location
-  resource_group_name = azurerm_resource_group.rg-block.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet-block.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_windows_virtual_machine" "vm-block" {
-
-  name                = var.vm-name
-  resource_group_name = azurerm_resource_group.rg-block.name
-  location            = azurerm_resource_group.rg-block.location
-  size                = "Standard_b2s"
-  admin_username      = "kunal"
-  admin_password      = var.vm-password
-  network_interface_ids = [
-    azurerm_network_interface.nic-block.id,
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
-    version   = "latest"
-  }
-}
-
